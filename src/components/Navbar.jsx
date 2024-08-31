178b9706d3f7db306ddd5562a177baf3b083e764
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2024-08-31 11:48:05 +0530

    Home page section 1 mobile version complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { ComLogo, menuClosed, menuOpen } from "../utils";
+import gsap from "gsap";
+import { useState } from "react";
+import { navlists } from "../constants";
+import { Link } from "@tanstack/react-router";
+
+const Navbar = () => {
+  const [clicked, setclicked] = useState(menuClosed);
+  const [display, setdisplay] = useState("hidden");
+
+  const handleClick = () => {
+    clicked === menuClosed ? setclicked(menuOpen) : setclicked(menuClosed);
+    if (clicked === menuClosed) {
+      setdisplay("block");
+      gsap.fromTo(
+        ".menuSec",
+        {
+          x: "150px",
+          opacity: 0,
+        },
+        {
+          x: "0",
+          opacity: 1,
+        }
+      );
+    } else {
+      gsap.fromTo(
+        ".menuSec",
+        {
+          x: "0",
+          opacity: 1,
+        },
+        {
+          x: "150px",
+          opacity: 0,
+        }
+      );
+      //   setdisplay("hidden");
+    }
+  };
+  return (
+    <nav className="w-screen h-[10vh] HeadLine text-white flex justify-center items-center  ">
+      <div className="h-full w-5/6 flex items-center justify-between  ">
+        <div id="logo" className="h-full flex  items-center">
+          <img src={ComLogo} className="" />
+        </div>
+        <div id="menu" className="h-full flex  items-center">
+          <img
+            src={clicked}
+            onClick={handleClick}
+            className="hover:scale-105 z-40 sm:hidden cursor-pointer h-1/2 transition-all duration-300"
+          />
+        </div>
+      </div>
+      <div
+        className={` fixed menuSec top-16 right-0   ${display} bg-white bg-opacity-60 backdrop-blur-md  border z-10 w-1/3 m-2 rounded-2xl h-52 sm:hidden  `}
+      >
+        <ul className="text-black Headline p-5 flex flex-col gap-4 text-lg">
+          {navlists.map((item, index) => (
+            <li
+              key={index}
+              className="hover:border-b-2 cursor-pointer border-[#4733C9] hover:text-[#4733C9] transition-all duration-75 "
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+const bg1 = "../src/assets/BG1.png";
+const bg2 = "../src/assets/MobView.png";
+const logo = "../src/assets/PrideLogo.svg";
+const menu = "../src/assets/menu.svg";
+const cross = "../src/assets/close.svg";
+
+export const BgSectionHomeDesk = bg1;
+export const BgSectionHomeMob = bg2;
+export const ComLogo = logo;
+export const menuClosed = menu;
+export const menuOpen = cross;
